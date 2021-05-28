0d7fe2f4f4101affd6d15917fcae4c036326c5bc
marpapadopoulou <maria@DESKTOP-1NMGO2J.localdomain>
2021-05-29 02:31:30 +0300

    Updates

---
+++ precache-manifest.7ef50a4604178b724da63857f00e636a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17148ec9ceb3f80c67451e214fb00f30",
+    "url": "/Auction-app/index.html"
+  },
+  {
+    "revision": "5e326eb0e112b34df032",
+    "url": "/Auction-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "82147bab9f8adb13931f",
+    "url": "/Auction-app/static/js/2.6566492f.chunk.js"
+  },
+  {
+    "revision": "5e326eb0e112b34df032",
+    "url": "/Auction-app/static/js/main.126eefba.chunk.js"
+  },
+  {
+    "revision": "f7472452af8151a2b3cc",
+    "url": "/Auction-app/static/js/runtime-main.0409cdc2.js"
+  }
+]);
